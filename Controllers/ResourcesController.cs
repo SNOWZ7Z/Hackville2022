afc816cc8ab7939df066cec511f8e0ae33756707
Marcelo Geldres <geldres.marcelo@gmail.com>
2022-01-15 11:40:36 -0500

    Resources Initial Layout
    Controllers and Views Added

---
+++ Controllers/ResourcesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackville2022.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GovernmentResources()
+        {
+            return View("GovernmentResources");
+        }
+
+        public IActionResult ClothingResources()
+        {
+            return View("ClothingResources");
+        }
+
+        public IActionResult HealthResources()
+        {
+            return View("HealthResources");
+        }
+
+        public IActionResult PrintingResources()
+        {
+            return View("PrintingResources");
+        }
+    }
+}
