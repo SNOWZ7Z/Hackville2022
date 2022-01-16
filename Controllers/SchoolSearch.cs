c69cb08bd18c4eb7f432732b33e78a22b48e93ca
Vladislav Mun <welastor@gmail.com>
2022-01-15 22:21:53 -0500

    added the calendar

--- Controllers/SchoolSearch.cs
+++ Controllers/SchoolSearch.cs
@@ -8,8 +8,14 @@ namespace Hackville2022.Controllers
 {
     public class SchoolSearch : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AcademicCalendar() { 
             return View();
         }
     }
